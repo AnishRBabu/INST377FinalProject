f3c06dc8fdf836ed24840a5450004c629860b6cb
Jason Li <jason@Jasons-MacBook-Pro-2.local>
2023-12-19 21:57:06 -0500

    (feat): quiz

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -8,13 +8,13 @@ function AboutUs() {
       <div className="card">
         <h2>Introduction / Background</h2>
         <p>
-          Embarking on a career in a new country should be an exciting adventure, but the complexities of finding overseas jobs can often turn it into a daunting challenge. Recognizing the need for a streamlined solution, we present (our company name). Our platform is born out of the understanding that job seekers, whether recent graduates or seasoned professionals, face unique hurdles when navigating the German job market. The idea germinated from the desire to simplify and enhance the job search experience, making it accessible, efficient, and tailored to individual preferences.
+          Embarking on a career in a new country should be an exciting adventure, but the complexities of finding overseas jobs can often turn it into a daunting challenge. Recognizing the need for a streamlined solution, we present GlobalCareerHub. Our platform is born out of the understanding that job seekers, whether recent graduates or seasoned professionals, face unique hurdles when navigating the German job market. The idea germinated from the desire to simplify and enhance the job search experience, making it accessible, efficient, and tailored to individual preferences.
         </p>
         <p>
-          At (our company name), we're not just a job portal; we're your partner in the quest for professional growth. With a keen focus on Germany, our platform harnesses the robust Arbeitnow API, ensuring that our users have access to a wealth of job opportunities across diverse industries. Beyond mere listings, we offer a comprehensive solution to those unsure about their career path. Our innovative Career Quiz guides users through a personalized exploration, matching skills and aspirations with suitable job profiles.
+          At GlobalCareerHub, we're not just a job portal; we're your partner in the quest for professional growth. With a keen focus on Germany, our platform harnesses the robust Arbeitnow API, ensuring that our users have access to a wealth of job opportunities across diverse industries. Beyond mere listings, we offer a comprehensive solution to those unsure about their career path. Our innovative Career Quiz guides users through a personalized exploration, matching skills and aspirations with suitable job profiles.
         </p>
         <p>
-          Our commitment extends to providing an inclusive space for anyone seeking employment in Germany. Whether you're navigating the intricacies of work visas, sponsorship, or remote opportunities, (our company name) is designed to be your ally. Join us in reshaping the narrative of job hunting, turning it into a journey of discovery, growth, and fulfillment.
+          Our commitment extends to providing an inclusive space for anyone seeking employment in Germany. Whether you're navigating the intricacies of work visas, sponsorship, or remote opportunities, GlobalCareerHub is designed to be your ally. Join us in reshaping the narrative of job hunting, turning it into a journey of discovery, growth, and fulfillment.
         </p>
       </div>
       <div className="card">
@@ -26,22 +26,22 @@ function AboutUs() {
           Job seekers, particularly recent graduates or those contemplating a career shift, find themselves overwhelmed by the fragmented information available. The lack of a centralized hub for overseas jobs in Germany hampers their ability to make informed decisions. Additionally, the diverse nature of employment options, including sponsored positions, visa-centric roles, and remote opportunities, necessitates a platform that goes beyond conventional job portals. 
         </p>
         <p>
-          The absence of a dedicated solution compounds the stress associated with job hunting in a foreign country. Recognizing this gap, (our company name) emerges as a response to the complex and multifaceted challenges faced by individuals in search of international employment in Germany. Our platform seeks to streamline this process, providing clarity, accessibility, and personalized guidance to make the pursuit of overseas jobs not just feasible but enriching.
+          The absence of a dedicated solution compounds the stress associated with job hunting in a foreign country. Recognizing this gap, GlobalCareerHub emerges as a response to the complex and multifaceted challenges faced by individuals in search of international employment in Germany. Our platform seeks to streamline this process, providing clarity, accessibility, and personalized guidance to make the pursuit of overseas jobs not just feasible but enriching.
         </p>
         <p>
-          By addressing these challenges, (our company name) aims to redefine the overseas job search experience, transforming it into a seamless journey of discovery and empowerment for job seekers of varied backgrounds and aspirations.
+          By addressing these challenges, GlobalCareerHub aims to redefine the overseas job search experience, transforming it into a seamless journey of discovery and empowerment for job seekers of varied backgrounds and aspirations.
         </p>
       </div>
       <div className="card">
         <h2>Our goal / Mission</h2>
         <p>
-          At (our company name), our mission is to revolutionize the landscape of overseas job searches, particularly in Germany, by offering an innovative and user-centric platform. We aspire to empower individuals at every stage of their career journey, providing them with the tools and resources needed to navigate the complexities of international employment. Our goal is to create a comprehensive and accessible hub that not only showcases a diverse array of job opportunities but also serves as a guiding companion, offering insights, advice, and personalized recommendations.
+          At GlobalCareerHub, our mission is to revolutionize the landscape of overseas job searches, particularly in Germany, by offering an innovative and user-centric platform. We aspire to empower individuals at every stage of their career journey, providing them with the tools and resources needed to navigate the complexities of international employment. Our goal is to create a comprehensive and accessible hub that not only showcases a diverse array of job opportunities but also serves as a guiding companion, offering insights, advice, and personalized recommendations.
         </p>
         <p>
-          We are committed to fostering a community where job seekers, from recent graduates to seasoned professionals, can find not just employment but a fulfilling career that aligns with their aspirations and values. By leveraging technology, data-driven insights, and a deep understanding of the overseas job market, we aim to simplify the entire job search process. Whether it's understanding visa requirements, exploring remote work options, or discovering industries that align with individual skill sets, (our company name) is designed to be a one-stop destination for a transformative overseas job search experience.
+          We are committed to fostering a community where job seekers, from recent graduates to seasoned professionals, can find not just employment but a fulfilling career that aligns with their aspirations and values. By leveraging technology, data-driven insights, and a deep understanding of the overseas job market, we aim to simplify the entire job search process. Whether it's understanding visa requirements, exploring remote work options, or discovering industries that align with individual skill sets, GlobalCareerHub is designed to be a one-stop destination for a transformative overseas job search experience.
         </p>
         <p>
-          Through our platform, we envision a future where the pursuit of international employment becomes a journey of growth, exploration, and meaningful connections. (our company name) is more than a job portal; it's a catalyst for career discovery, breaking down barriers, and opening doors to a world of professional possibilities. Join us in reshaping the narrative of overseas job searches and unlocking the doors to global career opportunities.
+          Through our platform, we envision a future where the pursuit of international employment becomes a journey of growth, exploration, and meaningful connections. GlobalCareerHub is more than a job portal; it's a catalyst for career discovery, breaking down barriers, and opening doors to a world of professional possibilities. Join us in reshaping the narrative of overseas job searches and unlocking the doors to global career opportunities.
         </p>
       </div>
     </div>
---
+++ src/components/Quiz.js
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import questions from "./questions"; 
+
+
+function Quiz() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [userResponses, setUserResponses] = useState({});
+
+
+  const handleResponse = (response) => {
+    setUserResponses({
+      ...userResponses,
+      [currentQuestion]: response,
+    });
+
+
+    setCurrentQuestion(currentQuestion + 1);
+  };
+
+
+  const renderQuestion = () => {
+    const categories = Object.keys(questions);
+
+
+    if (currentQuestion >= 10) {
+      return renderResults();
+    }
+
+
+    const currentCategory = categories[currentQuestion % categories.length];
+    const categoryQuestions = questions[currentCategory];
+
+
+    if (!categoryQuestions || currentQuestion >= categories.length * categoryQuestions.length) {
+      return renderResults();
+    }
+
+
+    const currentQuestionData = categoryQuestions[currentQuestion % categoryQuestions.length];
+
+
+    return (
+      <div>
+        <h1>{currentQuestionData.question}</h1>
+        {currentQuestionData.options.map((option, index) => (
+          <button key={index} onClick={() => handleResponse(option)}>
+            {option}
+          </button>
+        ))}
+      </div>
+    );
+  };
+
+  const renderResults = () => {
+    const categoryScores = Object.keys(questions).reduce((scores, category) => {
+      scores[category] = 0;
+  
+  
+      for (let i = 0; i < 10; i++) {
+        const response = userResponses[i];
+        if (response && questions[category][i % questions[category].length].options.includes(response)) {
+          scores[category]++;
+        }
+      }
+  
+      return scores;
+    }, {});
+  
+  
+    console.log("Category Scores:", categoryScores);
+  
+  
+    let maxCount = 0;
+    let maxCategory = "";
+  
+  
+    Object.keys(categoryScores).forEach((category) => {
+      const count = categoryScores[category];
+  
+  
+      if (count > maxCount) {
+        maxCount = count;
+        maxCategory = category;
+      }
+    });
+  
+  
+    console.log("Max Category:", maxCategory); 
+  
+  
+    return (
+      <div>
+        <h1>Congratulations!</h1>
+        <p>Your needs fit those of a {maxCategory} position!</p>
+      </div>
+    );
+  };
+  
+
+
+  return <div>{currentQuestion < 10 ? renderQuestion() : renderResults()}</div>;
+}
+
+
+export default Quiz;
+
+
+
+
+
+
+
+
+
+
+
